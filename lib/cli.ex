a2b9428af7c2aeff704841740ad763ed8ac3d96b
Ekaterina Krivich <ekaterinak@heathmont.net>
2018-12-07 09:26:30 +0200

    add one command

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,5 +1,36 @@
 defmodule ENaguEe.CLI do
+  @commands %{
+    "quit" => "Quit the CLI"
+  }
+
   def main(_args) do
-    IO.puts("Welcome")
+    print_welcome_message()
+    print_help_message()
+
+    receive_command()
   end  
+
+  defp print_welcome_message, do: IO.puts("Welcome to ENaguEe CLI\n")
+
+  defp print_help_message do
+    IO.puts("\nThe simulator supports following commands:\n")
+    @commands
+    |> Enum.map(fn({command, description}) -> IO.puts("#{command} - #{description}\n") end)
+  end
+
+  defp receive_command do
+    IO.gets("\n> ")
+    |> String.trim
+    |> String.downcase
+    |> execute_command
+  end
+
+  defp execute_command("quit") do
+    IO.puts "\nBye!"
+  end
+
+  defp execute_command(_) do
+    IO.puts "\nUnknown command"
+    receive_command()
+  end
 end
